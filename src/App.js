b5101f7e053dc9857e037257853884f808c1ec25
allanetizenm <allankosgei747@gmail.com>
2021-10-26 16:27:06 +0300

    adding tasks using forms

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import {useState } from 'react'
+import { useState } from 'react'
+import AddTask from "./components/AddTask";
 
 
-const App =() => {
-  
-  
+const App = () => {
+
+
   const [tasks, setTasks] = useState(
     [
-        {
-         id:1,
-         text:'Gym',
-         day:'Jan 1st 10:30',
-         reminder: true,
-        },
-        {
-         id:2,
-         text: 'Farming',
-         day:'Jan 7th 1:20am',
-         reminder:false,
-        },
-        {
-         id:3,
-         text: 'Movies',
-         day:'June 1st',
-         reminder: true,
-        }
-     ]
-)
+      {
+        id: 1,
+        text: 'Gym',
+        day: 'Jan 1st 10:30',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Farming',
+        day: 'Jan 7th 1:20am',
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: 'Movies',
+        day: 'June 1st',
+        reminder: true,
+      }
+    ]
+  )
+  //add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) +1
+    console.log(id)
+    const newTask = {id, ...task }
+    setTasks([...tasks, newTask])
+
+  }
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+
+  }
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) =>
+      task.id === id ? {
+        ...task, reminder:
+          !task.reminder
+      } : task
+    )
+    )
+  }
   return (
     <div className="container">
-      <Header/>
-      <Tasks tasks= {tasks}/>
+      <Header />
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Task to show :( '}
     </div>
-    
+
   );
 }
 
